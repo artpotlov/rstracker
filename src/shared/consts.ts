6728142e78a64c2bbe1d04e08c4f10d1f75063c0
Artem Potlov <potlov.artem@gmail.com>
2022-11-22 21:44:57 +0300

    feat: add links to github profiles

--- src/shared/consts.ts
+++ src/shared/consts.ts
@@ -1,1 +1,6 @@
 export const BASE_URL = process.env.REACT_APP_API_URL || 'https://rss-kanban-rest.up.railway.app';
+export const GITHUB_LINKS = {
+  artpotlov: 'https://github.com/artpotlov',
+  tonKristall: 'https://github.com/tonKristall',
+  slavaShchipunov: 'https://github.com/Slava-Shchipunov',
+};
