ad7b5afb0bc731f6f78e74ae4e9d01156643ae6e
Slava-Shchipunov <shchipunovsd@gmail.com>
2022-11-28 15:07:50 +0700

    refactor: change consts.ts according to the review

--- src/shared/consts.ts
+++ src/shared/consts.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.REACT_APP_API_URL || 'https://rss-kanban-rest.onrender.com';
+export const BASE_URL = process.env.REACT_APP_API_URL || 'https://rss-kanban-rest.up.railway.app';
