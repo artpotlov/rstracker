bcbd13da158e8f2add62c08bb794e4339ccad3b7
Artem Potlov <potlov.artem@gmail.com>
2022-12-01 11:34:53 +0300

    feat: add chakra provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { Outlet } from 'react-router-dom';
-import { Header } from 'components/Header/Header';
 import { Footer } from 'components/Footer';
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, ChakraProvider, Flex } from '@chakra-ui/react';
+import { Header } from './components/Header/Header';
 
 export const App = () => {
   return (
-    <Flex minH="100vh" flexDirection="column">
-      <Header />
-      <Box as="main" flexGrow="1" position="relative">
-        <Outlet />
-      </Box>
-      <Footer />
-    </Flex>
+    <ChakraProvider>
+      <Flex minH="100vh" flexDirection="column">
+        <Header />
+        <Box as="main" flexGrow="1" position="relative">
+          <Outlet />
+        </Box>
+        <Footer />
+      </Flex>
+    </ChakraProvider>
   );
 };
