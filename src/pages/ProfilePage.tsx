b9951ad97dd598124739928fcd864ee10de217ef
Slava-Shchipunov <shchipunovsd@gmail.com>
2022-12-02 03:24:18 +0700

    feat: add EditProfileForm.tsx

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,3 +1,9 @@
+import { PageGuard } from 'hoc/PageGuard';
+
 export const ProfilePage = () => {
-  return <div>edit user profile</div>;
+  return (
+    //<PageGuard>
+    <div>edit user profile</div>
+    //</PageGuard>
+  );
 };
