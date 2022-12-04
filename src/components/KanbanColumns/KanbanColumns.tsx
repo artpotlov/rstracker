7f3b27655877f9d8cc9ecaa401c02bd52e0dcf5f
Artem Potlov <potlov.artem@gmail.com>
2022-12-04 21:07:58 +0300

    feat: add task form

--- src/components/KanbanColumns/KanbanColumns.tsx
+++ src/components/KanbanColumns/KanbanColumns.tsx
@@ -17,6 +17,8 @@ import { useAppDispatch } from 'hooks/useAppDispatch';
 import { ConfirmModal } from 'components/ConfirmModal/ConfirmModal';
 import { ColumnBoard } from 'components/ColumnBoard/ColumnBoard';
 import { DRAGGABLE_TYPES } from 'shared/consts';
+import { useState } from 'react';
+import { CreateTaskForm } from '../CreateTaskForm/CreateTaskForm';
 
 export const KanbanColumns = () => {
   const { t } = useTranslation();
@@ -27,6 +29,12 @@ export const KanbanColumns = () => {
   const deletedColumn = selectDeletedColumn();
   const board = selectBoard();
   const { setDeletedColumn } = columnsActions;
+
+  const [isOpenTaskModal, setOpenTaskModal] = useState(false);
+
+  const handleToggleTaskModal = () => {
+    setOpenTaskModal((prev) => !prev);
+  };
 
   const confirmDeleteColumn = () => {
     if (deletedColumn && board) {
@@ -92,6 +100,13 @@ export const KanbanColumns = () => {
           question={`${t('columns.deleteQuestion')} ${deletedColumn?.title}?`}
           handleClose={handleCloseConfirm}
         />
+      </Portal>
+      <Portal
+        title={t('tasks.createTitle')}
+        handleClose={handleToggleTaskModal}
+        isOpen={isOpenTaskModal}
+      >
+        <CreateTaskForm handleClose={handleToggleTaskModal} />
       </Portal>
     </Box>
   );
