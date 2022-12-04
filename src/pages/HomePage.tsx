297bbaa623df94a64f6288b4d485059222eb46b2
Aleksander Shustov <krisale@yandex.ru>
2022-12-04 16:39:50 +0300

    feat: show error message

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,14 +6,27 @@ import { Box, Divider } from '@chakra-ui/react';
 import { TitlePage } from 'components/TitilePage/TitlePage';
 import { BoardsList } from 'components/BoardsList/BoardsList';
 import { useTranslation } from 'react-i18next';
+import { useAppToast } from 'hooks/useAppToast';
+import { selectErrorBoards } from 'store/boards/boards.selectors';
+import { boardsActions } from 'store/boards/boards.slice';
 
 export const HomePage = () => {
   const dispatch = useAppDispatch();
   const { t } = useTranslation();
+  const toast = useAppToast();
+  const errorBoards = selectErrorBoards();
+  const { setError } = boardsActions;
 
   useEffect(() => {
     dispatch(getAllBoardsThunk());
   }, [dispatch]);
+
+  useEffect(() => {
+    if (errorBoards) {
+      toast('error', t(`errors.${errorBoards}`));
+      dispatch(setError(''));
+    }
+  }, [errorBoards, toast, t, dispatch, setError]);
 
   return (
     <PageGuard>
