175fbca067aba3f5906eb4cb8a97fca39210d5b8
Artem Potlov <potlov.artem@gmail.com>
2022-12-05 18:36:36 +0300

    fix: return footer for all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,16 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { Header } from 'components/Header/Header';
 import { Footer } from 'components/Footer';
 import { Box, Flex } from '@chakra-ui/react';
-import { useCallback, useEffect, useState } from 'react';
-import { pathRoutes } from './router/router';
 
 export const App = () => {
-  const [isNotBoardPage, setBoardPage] = useState(true);
-  const matchPath = useLocation();
-  const checkPage = useCallback((path: string) => {
-    const match = new RegExp(pathRoutes.boards + '/\\w+$', 'i');
-    return !match.test(path);
-  }, []);
-
-  useEffect(() => {
-    setBoardPage(checkPage(matchPath.pathname));
-  }, [matchPath, checkPage]);
-
   return (
     <Flex minH="100vh" flexDirection="column">
       <Header />
       <Box as="main" flexGrow="1" position="relative">
         <Outlet />
       </Box>
-      {isNotBoardPage && <Footer />}
+      <Footer />
     </Flex>
   );
 };
